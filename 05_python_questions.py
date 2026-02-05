a126715a0a882223f7d02ebe34bd2b149ca045fe
Vaisakh-Nirupam <vaisakhnirupamvn@gmail.com>
2026-02-06 00:02:06 +0530

    Python Interview Questions

--- 05_python_questions.py
+++ 05_python_questions.py
@@ -8,6 +8,7 @@ print(common(str1,str2))
 common = lambda s1,s2 : s1 & s2
 print(common(str1,str2))
 
+
 # Word Frequency
 sen = "Sheena loves eating apple and mango. Her sister also loves eating apple and mango" 
 dit = {}
@@ -17,11 +18,13 @@ for i in sen.split():
     dit[i] += 1
 print(dit)
 
+
 # Merge 2 List as Key Value of Dict
 l1 = ['hi','how','hey']
 l2 = [1,2,3]
 dit = dict(zip(l1,l2))
 print(dit)
+
 
 # Find the Missing Number
 num = [1,2,4,5,7]
@@ -42,3 +45,61 @@ for i,num in enumerate(lst):
     if sub in dit:
         print(f"{dit[sub]},{i}")
     dit[num] = i
+
+
+# Minimum Difference bw each Elements
+lst = [5,32,45,4,12,18,25]
+print("List:",lst)
+lst.sort()
+print("Sorted List:",lst)
+min_val = float('inf')
+for i in range(len(lst)-1):
+    diff = lst[i+1] - lst[i] 
+    if diff < min_val:
+        min_val = diff
+print("Minimum Difference:",min_val)
+# or
+min_diff = min(b - a for a, b in zip(lst, lst[1:]))
+print("Minimum Difference:", min_diff)
+
+
+# Maximum Difference bw each Elements
+lst = [5,32,45,4,12,18,25]
+print("List:",lst)
+lst.sort()
+print("Sorted List:",lst)
+max_val = float('-inf')
+for i in range(len(lst)-1):
+    diff = lst[i+1] - lst[i] 
+    if diff > max_val:
+        max_val = diff
+print("Maximum Difference:",max_val)
+# or
+max_diff = max(b - a for a, b in zip(lst, lst[1:]))
+print("Maximum Difference:", max_diff)
+
+
+# Stack Push, Pop and Operate
+def stack_operate(lst):
+    op = ["+","-","*","/","%"]
+    stk = [] 
+    for i in lst:
+        if i not in op:
+            stk.append(int(i))
+        else:
+            n1, n2 = stk.pop(), stk.pop()
+            if i == '+':
+                stk.append(n1+n2)
+            elif i == '-':
+                stk.append(n1-n2)
+            elif i == '*':
+                stk.append(n1*n2)
+            elif i == '/':
+                stk.append(n1/n2)
+            elif i == '%':
+                stk.append(n1%n2)
+    return stk[-1]
+print(stack_operate(["1","2","+","3","*"]))
+
+
+# 
