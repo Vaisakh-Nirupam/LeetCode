c6ae25291adae87a5d12536c650f3c6eb05af616
Vaisakh-Nirupam <vaisakhnirupamvn@gmail.com>
2026-02-05 15:25:09 +0530

    Swap Zeroes to last index

---
+++ 01_swap_zero.py
@@ -0,0 +1,31 @@
+# Swap Zeroes to the last without changing the orders of the non-zero numbers
+
+lst = [0,1,0,3,12]
+print("Before: ",lst)
+
+# Mine
+mysol = lst.copy()
+for i in mysol:
+    if i == 0:
+        mysol.remove(0)
+        mysol.append(0)
+print("Mine After: ",mysol)
+
+# GPT
+gptsol = lst.copy()
+zero_count = gptsol.count(0)
+gptsol = [i for i in gptsol if i != 0] + [0]*zero_count
+print("GPT After: ",gptsol)
+
+# Yt
+ytsol = lst.copy()
+prev = 0
+for i in range(0,len(ytsol)):
+    if ytsol[i] != 0:
+        # swap
+        hold = ytsol[prev]
+        ytsol[prev] = ytsol[i]
+        ytsol[i] = hold
+        # increment
+        prev += 1
+print("Yt After: ",ytsol)
