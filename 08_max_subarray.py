62318bb0b080ac051d404b8844c1fc52b4c15a5f
Vaisakh-Nirupam <vaisakhnirupamvn@gmail.com>
2026-02-06 18:57:12 +0530

    Maximum Sum SubArray

--- 08_max_subarray.py
+++ 08_max_subarray.py
@@ -1,12 +1,12 @@
 # Maximum Sum SubArray. From the array, pick a continuous chunk (subarray) whose elements add up to the largest possible sum. (Kadane’s algorithm) 
 
-# def max_subarray_sum(arr):
-#     max_sum = 0
-#     for i in range(0,len(arr)-1):
-#         curr_sum = arr[i]+arr[i+1]
-#         max_sum = max(curr_sum,max_sum)
-#     return max_sum
-# print(max_subarray_sum([4,-3,-2,2,3,1,-2,-3,6,-6,-4,2,1]))
+def max_sum(arr):
+    max_sum = 0
+    for i in range(0,len(arr)-1):
+        curr_sum = arr[i]+arr[i+1]
+        max_sum = max(curr_sum,max_sum)
+    return max_sum
+print(max_subarray_sum([4,-3,-2,2,3,1,-2,-3,6,-6,-4,2,1]))
 
 def max_subarray_sum(arr):
     current_sum = arr[0]
